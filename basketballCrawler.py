512735f3ea04f2a5a9deac488853b5538453cd6c
ab15 <8379417+ab15@users.noreply.github.com>
2018-08-11 13:20:59 -0700

    Update basketballCrawler.py
    another version of next

--- basketballCrawler.py
+++ basketballCrawler.py
@@ -30,8 +30,7 @@ def getCurrentPlayerNamesAndURLS(suppressOutput=True):
         # we know that all the currently active players have <strong> tags, so we'll limit our names to those
         current_names = letter_page.findAll('strong')
         for n in current_names:
-            name_data = n.children.iterator.__next__()
-            try:
+            name_data = = next(n.children)            try:
                 names.append((name_data.contents[0], 'http://www.basketball-reference.com' + name_data.attrs['href']))
             except Exception as e:
                 pass
